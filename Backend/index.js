65eb8f81af06ce7017dc2ac486e73fbb03340dd0
fahim <fahimahamedweb@gmail.com>
2025-01-26 08:06:50 +0600

    mongodb added

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,19 @@
 import express from "express";
-//pass incoming data
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 
 const port = process.env.PORT || 5000;
 const app = express()
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb" }));
 async function main() {
+  await mongoose.connect(process.env.DB_URL);
 
+  
+  
   
    
     app.get("/", (req, res) => {
