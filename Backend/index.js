a35585d91267055814731406eecd701aadcf6828
fahim <fahimahamedweb@gmail.com>
2025-01-29 15:50:39 +0600

    product routes added

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoutes from "./users/user.route.js"
+import productRoutes from "./products/product.route.js"
 dotenv.config();
 import cors from "cors";
 const app = express()
@@ -16,6 +17,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb" }));
 app.use("/api/authRoutes", authRoutes);
+app.use("/api/productRoutes", productRoutes);
 async function main() {
   await mongoose.connect(process.env.DB_URL);
     app.get("/", (req, res) => {
---
+++ Backend/products/products.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    oldPrice: {
+        type: Number,
+    },
+    image: {
+        type: String,
+    },
+    color: {
+        type: String,
+    },
+    ratings: {
+        type: Number,
+        default: 0,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    }
+});
+
+const Products = mongoose.model("Product", productSchema);
+export default Products;
